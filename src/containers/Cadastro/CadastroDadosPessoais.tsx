c5c18e61904fc40c733e10e4f93f85af03af552a
Nicole Anater <nicoleanater@gmail.com>
2020-11-26 05:21:28 -0300

    Adding ImagePicker library.

--- src/containers/Cadastro/CadastroDadosPessoais.tsx
+++ src/containers/Cadastro/CadastroDadosPessoais.tsx
@@ -2,10 +2,12 @@ import { useNavigation } from '@react-navigation/native';
 import { StackHeaderProps } from '@react-navigation/stack';
 import React, { FunctionComponent, useLayoutEffect } from 'react';
 import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { HeaderCadastro } from '../../components/HeaderCadastro/HeaderCadastro';
 import { TipoUsuario } from '../../models/Usuario';
 import { useStore } from '../../store';
 import styles from './CadastroDadosPessoaisStyles';
+import ImagePicker from 'react-native-image-picker';
 interface IProps {
     // Props type definition
 }
@@ -25,10 +27,25 @@ export const CadastroDadosPessoais: FunctionComponent <IProps> = (props) => {
 			});
 		}, [navigation]);
 
+		const openGalery = () => {
+			ImagePicker.launchImageLibrary({
+				mediaType: 'photo',
+				// includeBase64: false,
+				// maxHeight: 200,
+				// maxWidth: 200
+			}, 
+			(response) => {console.log({response})})
+		}
+
 
     return (
         <View>
 					<Text>Cadastro</Text>
+					<View> 
+						<TouchableOpacity onPress={openGalery}>
+							<Text>Foto</Text>
+						</TouchableOpacity>
+					</View>
         </View>
     );
 };
