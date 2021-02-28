cf797b340ff6d6e91c3cfa50bdff23d98a8b3dce
Nicole Anater <nicoleanater@gmail.com>
2021-02-28 00:54:00 -0300

    Removing unnecessary nesting.

--- src/navigation/AuthStack/AuthStack.tsx
+++ src/navigation/AuthStack/AuthStack.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createStackNavigator } from "@react-navigation/stack";
-import MenuDrawer from "../DrawerNavigator/DrawerNavigator";
+import React from "react";
+import CompromissoScreen from '../../containers/CompromissoScreen/CompromissoScreen';
+import HomeScreen from '../../containers/HomeScreen/HomeScreen';
+import DrawerContent from "../DrawerContent/DrawerContent";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerStack = () => {
+	return (
+		<Stack.Navigator initialRouteName="DrawerNavigator">
+			<Stack.Screen name="Compromisso" component={CompromissoScreen} />
+      <Stack.Screen name="Feed" component={HomeScreen} />
+		</Stack.Navigator>
+	);
+}
 
 function AuthStack() {
 	return (
-		<Stack.Navigator initialRouteName="DrawerNavigator" screenOptions={{ headerShown: false }}>
-			<Stack.Screen name="DrawerNavigator" component={MenuDrawer} />
-		</Stack.Navigator>
+		<Drawer.Navigator initialRouteName="DrawerStackNavigator" drawerContent={props => <DrawerContent {...props}/>}>
+			<Drawer.Screen name="DrawerStackNavigator" component={DrawerStack} />
+		</Drawer.Navigator>
 	);
 }
 
