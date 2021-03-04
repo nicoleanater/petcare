80e9662b507e0cabbd3df0cc7e7539ab0b0a9e64
Nicole Anater <nicoleanater@gmail.com>
2021-03-04 02:49:49 -0300

    Using set interval to update timer value. Working if starting a new clock. Still needs to develop if clock was already started.

--- src/components/CollapseAgendamentos/CollapseAgendamentos.tsx
+++ src/components/CollapseAgendamentos/CollapseAgendamentos.tsx
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useRef, useState } from 'react';
 import { Text, View } from 'react-native';
 import Collapsible from 'react-native-collapsible';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
@@ -14,26 +14,63 @@ interface IProps {
 
 interface IState {
 	agendamentoIsCollapsed: Array<boolean>;
+	agendamentoTimers: Array<number>;
+	timersStatuses: Array<'initial' | 'playing' | 'finished'>;
 }
 
 export const CollapseAgendamentos: FunctionComponent<IProps> = ({ agendamentos }) => {
+	const clockCallRef = useRef(agendamentos.map((item, i) => null));
+
 	const initialState: IState = {
-		agendamentoIsCollapsed: agendamentos.map((item,i) => i === 0 ? false : true)
+		agendamentoIsCollapsed: agendamentos.map((item, i) => i === 0 ? false : true),
+		agendamentoTimers: agendamentos.map((item, i) => 0), // update to display difference between already set and now
+		timersStatuses: agendamentos.map((item, i) => 'initial') // update to conditionally set depending on time already set
 	};
 	const [agendamentoIsCollapsed, setAgendamentoIsCollapsed] = useState(initialState.agendamentoIsCollapsed);
+	const [agendamentoTimers, setAgendamentoTimers] = useState(initialState.agendamentoTimers);
+	const [timersStatuses, setTimersStatuses] = useState(initialState.timersStatuses);
 
 	const onCollapseHeaderPressed = (index: number) => {
 		setAgendamentoIsCollapsed((previousState) => {
 			console.log(previousState);
-			const newState = Object.assign([], previousState, {[index]: !previousState[index]});
+			const newState = Object.assign([], previousState, { [index]: !previousState[index] });
 			return newState;
 		})
 	}
 
-	const onTimerBadgePressed = (index: number) => {
-		console.log({index});
+	const startTimer = (index: number) => {
+		console.log({test: moment().format()});
 
-		//todo
+		clockCallRef.current[index] = setInterval(() => {
+			setAgendamentoTimers((previousState) => {
+				return Object.assign([], previousState, { [index]: previousState[index] + 1 });
+			})
+		}, 1000);
+	}
+
+	const stopTimer = (index: number) => {
+		clearInterval(clockCallRef.current[index]);
+	}
+
+	const onTimerBadgePressed = (index: number) => {
+		if (timersStatuses[index] === 'initial') {
+			startTimer(index);
+			setTimersStatuses((previousState) => {
+				return Object.assign([], previousState, { [index]: 'playing' });
+			})
+		} else if (timersStatuses[index] === 'playing') {
+			stopTimer(index);
+			setTimersStatuses((previousState) => {
+				return Object.assign([], previousState, { [index]: 'finished' });
+			})
+		}
+
+	}
+
+	const formatTime = (time: number) => {
+		return moment("2015-01-01").startOf('day')
+    .seconds(time)
+    .format('HH:mm:ss');
 	}
 
 	return (
@@ -43,13 +80,13 @@ export const CollapseAgendamentos: FunctionComponent<IProps> = ({ agendamentos }
 				{agendamentos.map((agendamento, i) => (
 					<View key={i}>
 						<TouchableWithoutFeedback onPress={() => onCollapseHeaderPressed(i)} style={styles.collapseHeader}>
-							<Icon name={'today'} style={styles.calendarIconStyle} size={20}/>
+							<Icon name={'today'} style={styles.calendarIconStyle} size={20} />
 							<Text style={styles.collapseHeaderText}>{moment(agendamento.data, 'YYYY-MM-DD').format('DD/MM/YYYY')}</Text>
-							<Icon name={'keyboard-arrow-down'} style={styles.arrowIconStyle} size={25}/>
+							<Icon name={'keyboard-arrow-down'} style={styles.arrowIconStyle} size={25} />
 						</TouchableWithoutFeedback>
-						<Collapsible collapsed={agendamentoIsCollapsed[i]} style={[styles.collapseContent, i === agendamentos.length-1 && {borderBottomWidth: 0}]}>
+						<Collapsible collapsed={agendamentoIsCollapsed[i]} style={[styles.collapseContent, i === agendamentos.length - 1 && { borderBottomWidth: 0 }]}>
 							<Text style={styles.collapseContentText}>{`13:34 / 14:28`}</Text>
-							<TimerBadge index={i} status={'initial'} time={`00:00:00`} onPress={onTimerBadgePressed}/>
+							<TimerBadge index={i} status={timersStatuses[i]} time={formatTime(agendamentoTimers[i])} onPress={onTimerBadgePressed} />
 						</Collapsible>
 					</View>
 				))}
