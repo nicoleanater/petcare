dd10107deb845ae0bf0ae872e588abbaeb5d388d
Nicole Anater <nicoleanater@gmail.com>
2021-03-08 17:49:44 -0300

    Updating Reactotron to work with physical devices.

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,12 @@
 import Reactotron from 'reactotron-react-native'
 import AsyncStorage from '@react-native-community/async-storage';
+import { NativeModules } from 'react-native';
+
+const host = NativeModules.SourceCode.scriptURL.split('://')[1].split(':')[0];
 
 Reactotron
   .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure() // controls connection & communication settings
+  .configure({ host }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
 	.connect() // let's connect!
 	
