3e0966f1e464232cbd798ebe97d0003cd0665d35
Nicole Anater <nicoleanater@gmail.com>
2021-03-10 20:01:52 -0300

    Showing loading when searching api. Added styled header.

--- src/containers/MensagensDetailsScreen/MensagensDetailsScreen.tsx
+++ src/containers/MensagensDetailsScreen/MensagensDetailsScreen.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect, FunctionComponent } from 'react';
-import { Text, View } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/core';
+import { StackHeaderProps } from '@react-navigation/stack';
+import React, { useEffect, FunctionComponent, useState, useLayoutEffect } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { AuthHeader } from '../../components/AuthHeader/AuthHeader';
+import { Mensagem } from '../../models/Mensagem';
 import styles from './MensagensDetailsScreenStyles';
 
 interface IProps {
@@ -12,17 +16,51 @@ interface IProps {
 }
 
 interface IState {
-	// State type definition
+	messages: Array<Mensagem>;
 }
 
 export const MensagensDetailsScreen: FunctionComponent<IProps> = (props) => {
 	const currentUserId = props.route.params?.currentUserId;
 	const otherUserId = props.route.params?.otherUserId;
+	const navigation = useNavigation();
+	const isFocused = useIsFocused();
+
+	const initialState: IState = {
+		messages: null
+	};
+
+	const [messages, setMessages] = useState(initialState.messages);
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			header: (props: StackHeaderProps) => <AuthHeader {...props} title={"Mensagens"} theme={'light'} />,
+		});
+	}, [navigation]);
+
+	useEffect(() => {
+		if (isFocused) {
+			getMessages();
+		}
+	}, [isFocused]);
+
+	const getMessages = async () => {
+		// try {
+		// 	const res = await buscaMensagens(usuario.id);
+		// 	getMessages(res.data);
+		// } catch (error) {
+		// 	console.error({error});
+		// }
+	}
 
 	console.log({currentUserId, otherUserId});
 
-	const initialState: IState = {
-	};
+	if (messages == null) {
+		return (
+			<View style={[styles.verticalCenterAlignedColumn, styles.marginTop]}>
+				<ActivityIndicator size='large'/>
+			</View>
+		);
+	}
 
 	return (
 		<View>
